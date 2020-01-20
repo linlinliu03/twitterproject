6336630b7258ae0d2ad46d638a946b12cf83fb65
linlinliu <linlinliu03@gmail.com>
2020-01-19 23:41:57 -0800

    first commit

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://linda:VsOGixulk8dIZ4Qj@cluster0-dyv33.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'VrLmW1TsLn'
+    //Make sure this is your own unique string
+}
