e99075e42cd65babefe0b93121a19bf2a4aa1b7b
linlinliu <linlinliu03@gmail.com>
2020-06-03 18:06:12 -0700

    Fix JWT token for user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,31 +24,39 @@ router.post('/register', (req, res) => {
     if (!isValid) {
         return res.status(400).json(errors);
     }
-    // Check to make sure nobody has already registered with a duplicate email
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (user) {
-                // Throw a 400 error if the email address already exists
-                return res.status(400).json({ email: "A user has already registered with this address" })
-            } else {
-                // Otherwise create a new user
-                const newUser = new User({
-                    handle: req.body.handle,
-                    email: req.body.email,
-                    password: req.body.password
-                })
 
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
-                        newUser.password = hash;
-                        newUser.save()
-                            .then(user => res.json(user))
-                            .catch(err => console.log(err));
-                    })
-                })
-            }
-        })
+    User.findOne({ handle: req.body.handle }).then(user => {
+        if (user) {
+            errors.handle = "User already exists";
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser·
+                        .save()
+                        .then(user => {
+                            const payload = { id: user.id, handle: user.handle };
+
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
 })
 
 router.post('/login', (req, res) => {
